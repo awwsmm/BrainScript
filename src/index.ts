f245a1656f3799656760ac2e462645295232d49c
Andrew Watson <aww@awwsmm.com>
2020-05-25 15:21:52 +0100

    update dependency locks

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export { readUTF32Char, brain, bf } from './Brain'
 export { Token, isToken } from './Token'
+
+// to publish:
+//   $ npx tsc
+//   $ git add .
+//   $ git commit -am <message>
+//   $ npm version <major/minor/patch>
+//   $ npm publish
+//   $ git push
